8092bcb23918d4263173e639536c3590fe7c0ee6
mmcgraw73 <shout@mcgraw.io>
2023-10-09 15:40:51 -0400

    Initial commit

---
+++ src/components/Img2Svg.vue
@@ -0,0 +1,11 @@
+<template></template>
+
+<script>
+export default {
+  name: 'SVGr',
+  props: {
+    imgs: [],
+    svgs: [],
+  }
+}
+</script>
---
+++ src/components/ImgEl.vue
@@ -0,0 +1,14 @@
+<template>
+
+</template>
+<script>
+export default = {
+  name: 'ImgEl',
+  props: {
+    svgdata: {
+      type: String
+    }
+  }
+}
+</script>
+<style></style>
