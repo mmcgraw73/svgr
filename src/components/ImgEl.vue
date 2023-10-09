674fb2252797b88f5f39e0ada81a0dae4f3c5ab9
mmcgraw73 <shout@mcgraw.io>
2023-10-09 17:39:42 -0400

    imgel component / wip

--- src/components/ImgEl.vue
+++ src/components/ImgEl.vue
@@ -1,14 +1,20 @@
 <template>
 
 </template>
+import { defineComponent } from "vue"
 <script>
-export default = {
+export default defineComponent ({
   name: 'ImgEl',
   props: {
     svgdata: {
       type: String
     }
+  },
+  computed: {
+    svgstr() {
+      return this.svgdata || null
+    }
   }
-}
+})
 </script>
 <style></style>
